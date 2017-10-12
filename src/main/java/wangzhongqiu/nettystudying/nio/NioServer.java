de8f85bffc14a000c94195cd0f2b2f115f1eff02
leon66666 <1902518544@qq.com>
2017-10-12 18:09:21 +0800

    first version

--- src/main/java/wangzhongqiu/nettystudying/nio/NioServer.java
+++ src/main/java/wangzhongqiu/nettystudying/nio/NioServer.java
@@ -1,11 +1,21 @@
 package wangzhongqiu.nettystudying.nio;
 
+import java.io.IOException;
+
 /**
  * Created by wangzhongqiu on 2017/9/30.
  */
 public class NioServer {
     public static void main(String[] args) throws IOException {
         int port = 8080;
-        
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        MultiplexerServer server = new MultiplexerServer(port);
+        new Thread(server, "NIO-MultiplexerServer-001").start();
     }
 }
