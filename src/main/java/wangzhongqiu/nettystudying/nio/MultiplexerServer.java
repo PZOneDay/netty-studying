fc6c1a8a33c7c318f35fe37a97fe66eaacecd7df
leon66666 <1902518544@qq.com>
2017-10-13 16:32:35 +0800

    first version

--- src/main/java/wangzhongqiu/nettystudying/nio/MultiplexerServer.java
+++ src/main/java/wangzhongqiu/nettystudying/nio/MultiplexerServer.java
@@ -12,6 +12,15 @@ import java.util.Set;
 
 /**
  * Created by wangzhongqiu on 2017/10/9.
+ * 使用NIO编程的优点总结如下：
+ * <p/>
+ * 1)  客户端发起的连接操作是异步的，可以通过在多路复用器注册OP_CONNECT等待后续结果，不需要像之前的客户端那样被同步阻塞；
+ * <p/>
+ * 2) SocketChannel的读写操作都是异步的，如果没有可读写的数据它不会同步等待，直接返回，这样IO通信线程就可以处理其它的链路，不需要同步等待这个链路可用；
+ * <p/>
+ * 3)  线程模型的优化：由于JDK的Selector在Linux等主流操作系统上通过epoll实现，它没有连接句柄数的限制（只受限于操作系统的最大句柄数或者对单个进程的句柄限制），这意味着一个Selector线程可以同时处理成千上万个客户端连接，而且性能不会随着客户端的增加而线性下降，因此，它非常适合做高性能、高负载的网络服务器。
+ * <p/>
+ * JDK1.7升级了NIO类库，升级后的NIO类库被称为NIO2.0，引人注目的是Java正式提供了异步文件IO操作，同时提供了与Unix网络编程事件驱动IO对应的AIO
  */
 public class MultiplexerServer implements Runnable {
     private Selector selector;
