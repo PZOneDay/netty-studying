9eab7ae2391cf996316643a24cfa812b12ad661d
leon66666 <1902518544@qq.com>
2018-01-06 17:12:12 +0800

    first version

---
+++ src/test/java/wangzhongqiu/nettystudying/Test.java
@@ -0,0 +1,11 @@
+package wangzhongqiu.nettystudying;
+
+/**
+ * @author wangzhongqiu
+ * @date 2018/1/6.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("master");
+    }
+}
